7722d484c5b7efb782dde0463af86052ff77d16b
r-butl <08lbutler31@gmail.com>
2025-04-25 13:04:23 -0700

    double buffer for input

--- src/services/read_frame.cpp
+++ src/services/read_frame.cpp
@@ -3,32 +3,8 @@
 #include "read_frame.hpp"
 #include <iostream>
 #include <string.h>
-
-FrameBuffer::FrameBuffer(size_t size)
-    : bufferSize(size), latestIndex(0), frameReady(false) {
-    buffer.resize(size);
-}
-
-void FrameBuffer::addFrame(const cv::Mat& frame) {
-    std::unique_lock<std::mutex> lock(bufferMutex);
-    buffer[latestIndex] = frame.clone(); // Store a copy of the frame
-    latestIndex = (latestIndex + 1) % bufferSize;
-    frameReady = true;
-    frameAvailable.notify_all(); // Notify waiting threads
-}
-
-cv::Mat FrameBuffer::getLatestFrame() {
-    std::unique_lock<std::mutex> lock(bufferMutex);
-    while (!frameReady) {
-        frameAvailable.wait(lock); // Wait for a new frame
-    }
-    return buffer[(latestIndex + bufferSize - 1) % bufferSize].clone(); // Return the latest frame
-}
-
-bool FrameBuffer::isFrameAvailable() {
-    std::lock_guard<std::mutex> lock(bufferMutex);
-    return frameReady;
-}
+#include "frame_buffer.hpp"
+#include <opencv2/opencv.hpp>
 
 void* frameReaderThread(void* arg) {
     FrameReaderArgs* args = static_cast<FrameReaderArgs*>(arg);
@@ -54,7 +30,7 @@ void* frameReaderThread(void* arg) {
             std::cerr << "Error: Empty frame captured." << std::endl;
             break;
         }
-        frameBuffer->addFrame(frame); // Add the frame to the buffer
+        frameBuffer->updateFrame(frame); // Add the frame to the buffer
     }
 
     cap.release();
